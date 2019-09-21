9ee691c665e2209cf019fee1d0939cc869f2f1ca
joan <joanjoseolivarria@gmail.com>
2019-09-21 09:12:44 -0600

    Links App crudo

---
+++ src/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    
+    database: {
+    
+        host: 'localhost',
+        user: 'root',
+        password: 'olivarria2095',
+        database: 'database_links'
+
+    }
+};
